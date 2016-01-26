44e8424b4cb9b472c37e13ddf0a9db7301a3efc8
Daniel Williams <mail@daniel-williams.co.uk>
2016-01-26 10:25:05 +0000

    Bump version number.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='otter-report',
-    version='0.0.10',
+    version='0.1.0',
     description="The simple HTML report generator for Python jobs.",
     long_description=readme + '\n\n' + history,
     author="Daniel Williams",
